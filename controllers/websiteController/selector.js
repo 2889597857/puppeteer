980cba3af069205c69767dfeb0be20904d2870b3
zhandehuang <2889597857@qq.com>
2023-03-02 17:21:30 +0800

    添加 page选择器接口

--- controllers/websiteController/selector.js
+++ controllers/websiteController/selector.js
@@ -5,10 +5,25 @@ async function addSelector(req, res) {
   const {} = req.query;
 }
 async function addLinkSelector(req, res) {}
-async function addContentSelector(req, res) {}
+async function addPageSelector(req, res) {
+  const { _id, selector } = req.body;
+  try {
+    if (!verifyID(_id)) throw new Error('_id is wrong');
+    const data = await WebsiteModel.findByIdAndUpdate(
+      _id,
+      {
+        $push: { pageSelector: { ...selector } },
+      },
+      { new: true }
+    );
+    res.json({ code: 200 });
+  } catch (error) {
+    res.json({ error:error.message  });
+  }
+}
 
 module.exports = {
   addSelector,
   addLinkSelector,
-  addContentSelector,
+  addPageSelector,
 };
