a3bf52e6055de31b0e881d6aa56271ccac3d21e0
zhandehuang <2889597857@qq.com>
2023-03-08 16:56:19 +0800

    youhua

--- models/user.js
+++ models/user.js
@@ -2,7 +2,14 @@ const mongoose = require('../config/mongoose');
 
 const UserSchema = mongoose.Schema({
   username: { type: String },
-  password: { type: String },
+  password: {
+    type: String,
+    /**
+     * 查询结果默认不返回密码
+     * find().select("+password")
+     */
+    select: false,
+  },
   role: {
     type: String,
     default: 'user',
